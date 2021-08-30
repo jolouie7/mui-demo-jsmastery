25cacef6175b63ba84604a30ffc908ce5af82870
JosephLouie <joseph161216@gmail.com>
2021-08-29 21:09:08 -0700

    Add button and styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@material-ui/core"
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@material-ui/core"
 import {PhotoCamera} from "@material-ui/icons";
 
+import useStyles from "./styles";
+
 function App() {
+  const classes = useStyles();
   return (
     <>
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <PhotoCamera />
-          <Typography variant="h6">
-            Photo Album
-          </Typography>
+          <PhotoCamera className={classes.icon}/>
+          <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution of letters, as opposed to using 'Content here,
+              content here', making it look like readable English.
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary Action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          
+        </Container>
+      </main>
     </>
   );
 }
